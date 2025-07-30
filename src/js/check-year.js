4337db14d55cc4bc5ee9e6de26a9ff47b616949c
KirillKaraisenli <146224890+KirillKaraisenli@users.noreply.github.com>
2025-07-30 18:41:27 +0300

    +icon

--- src/js/check-year.js
+++ src/js/check-year.js
@@ -11,10 +11,9 @@ function onBtnClick(e) {
         result.style.color="#039900"
     } else if (form.year.value % 4 !== 0 && form.year.value > 0) {
         result.textContent = 'Ви народилися не високосний рік';
-        result.style.color="#990000"
+        result.style.color="#990000"    
     } else {
         result.textContent = 'Будь ласках введіть коректний рік.';
           result.style.color="#990000"
     }
-    
 }   
