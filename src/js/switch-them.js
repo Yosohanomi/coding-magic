08509d151e20cb170b8f2b2267316e6fb7503af6
Лисенко Софія <146013576+Yosohanomi@users.noreply.github.com>
2025-08-08 20:57:17 +0300

    input + light-border vers for btn

--- src/js/switch-them.js
+++ src/js/switch-them.js
@@ -2,6 +2,7 @@ import whiteIcon from "/img/svg/white.svg";
 import darkIcon from "/img/svg/dark.svg";
 
 const icon = document.getElementById("icon");
+const inputBtn = document.querySelectorAll('.light-border')
 
 icon.addEventListener("click", () => {
     document.body.classList.toggle("dark-theme")
@@ -9,6 +10,10 @@ icon.addEventListener("click", () => {
     img.forEach(image => {
         image.classList.toggle('change-color');
     });
+
+    inputBtn.forEach(btn => {
+        btn.style.filter = 'drop-shadow(2px 2px 0px rgba(255, 255, 255, 0.226))';
+    })
 
     if (document.body.classList.contains("dark-theme")) {
 icon.src = darkIcon
